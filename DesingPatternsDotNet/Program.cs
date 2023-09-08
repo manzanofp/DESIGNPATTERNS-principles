092e2957ce080fd7e48ca4f1de28ee942b602b0a
manzanofp <felipemanzanofp@gmail.com>
2023-09-08 18:56:04 -0300

    feature: prototype design pattern

---
+++ DesingPatternsDotNet/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesingPatternsDotNet.Prototype;
+
+Customer customer1 = new Customer() { Name = "Customer 1" };
+Customer customer2 = customer1;
+
+customer2.Name = "Customer 2";
+
+Console.WriteLine($"Customer 1:{ customer1.Name }");
+Console.WriteLine($"Customer 2:{customer2.Name}");
+
+Console.Read();
---
+++ DesingPatternsDotNet/Prototype/Customer.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DesingPatternsDotNet.Prototype
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public Address Address { get; set; } = new Address();
+
+
+    }
+
+    [Serializable]
+
+    public  class Address
+    {
+        public string Street { get; set; }
+        public string Number { get; set; }
+    }
+}
